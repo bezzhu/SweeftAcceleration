ea8f19f68644b8765e74a7c6dfac17f453b52c1f
bezzhu <bezhuashvilibeka@gmail.com>
2023-03-15 18:24:00 +0400

    ვამატებ ამოცანა 3,4,5 ამოხსნებს და ტესტებს.

--- src/JavaTasks.java
+++ src/JavaTasks.java
@@ -1,7 +1,7 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class JavaTasks {
+
 
     /*
         ამოცანა 1.
@@ -9,8 +9,10 @@ public class JavaTasks {
             გადავუყვები მასივს და singleNumber ცვლადზე ვასრულებ XOR ოპერაციას.
             ორი ერთნაირი რიცხვი ამ ოპერაციისას გააქრობს ერთმანეთ და
             დარჩენბა რიცხვი, რომელიც არ მეორდება მასივში.
+            Time complexity - O(n).
+            Memory complexity - O(1).
     */
-    public int singleNumber(int[] nums){
+    public int singleNumber(int[] nums) {
         int singleNumber = 0;
 
         for (int num : nums) {
@@ -18,6 +20,7 @@ public class JavaTasks {
         }
         return singleNumber;
     }
+
     /*
         ამოცანა 1.
             ვიყენებ Set-ს ვინაიდან ის ინახავს განსხვავებულ მნიშვნელობებს.
@@ -25,6 +28,8 @@ public class JavaTasks {
             არა Set-ში. თუ არის წავშლი ამ რიცხვს Set-იდან და თუ არ არის
             დავამატებ Set-ში. ამრიგად ბოლოს დაგვრჩება რიცხვი რომელიც არ
             მეორდება მასივში.
+            Time complexity - O(n).
+            Memory complexity - O(1).
     */
     public int singleNumber2(int[] nums) {
         Set<Integer> set = new HashSet<>();
@@ -38,16 +43,19 @@ public class JavaTasks {
         return set.iterator().next();
     }
 
+
     /*
         ამოცანა 2.
             ვიყენებ მასივს სადაც კლებადობის მიხედვით ვინახავ მონეტებს.
             შემომაქვს ცვლადი დასახურდავებლად საჭირო მონეტების დასათვლელად.
-            გადავუყვები მასვივს და ცვლადს ვუმატებ დასახურდევებელი თანხის
+            გადავუყვები მასვივს და ცვლადს ვუმატებ დასახურდავებელი თანხის
             "განაყოფს" მასივის წევრზე. ვინაიდან int ების გაყოფაა დააბრუნებს
             მთელ მნიშვნელობას მაგ: 178/50=3. შემდეგ ვნაშთავ დასახურდევებელ
             თანხას და თუ ის გაუტოლდება ნულს მაშინ დაბრუნდება მონეტების რაოდენობას.
+            Time complexity - O(1).
+            Memory complexity - O(1).
         */
-    public int minSplit(int amount){
+    public int minSplit(int amount) {
         int[] coins = {50, 20, 10, 5, 1};
         int coinCount = 0;
         for (int coin : coins) {
@@ -59,5 +67,132 @@ public class JavaTasks {
         }
         return coinCount;
     }
+
+    /*
+        ამოცანა 3.
+            ვასორტირებ მასივს.
+            ცვლადში ვინახავ მინიმალურ დადებით რიცხვს 1-ს და
+            ვადარებ მასივის ყველა წევრს. თუ რომელიმეს გაუტოლდა
+            ცვლადს ვზრდი ერთით. ვინაიდან ისეთ რიცხვს ვეძებთ,
+            რომელიც ამ მასივში არ არის.
+            Time complexity - O(n*Log(n)).
+            Memory complexity - O(1).
+        */
+
+    public int notContains(int[] array) {
+        Arrays.sort(array);
+        int minNum = 1;
+        for (int num : array) {
+            if (num == minNum) {
+                minNum++;
+            }else if(num > minNum) return minNum;
+        }
+        return minNum;
+    }
+    /*
+        ამოცანა 4.
+            თავდაპირველად ვაშორებ სტრინგებს ნულებს,რომლებიც
+            სადაწყისში უწერია (თუ არის ასეთი შემთხვევა).
+            შეკრებას ვიწყებ თითოეული რიცხვის მარჯვენა ბოლოდან და
+            ვამატებთ შექმნი ცვლადს carry. მიღებულ მნიშვნელობას
+            დანაშთლს 2-ზე ვამატებთ სტრინგში.
+            ასე გადმოვუყვებით მარცხნივ მანამ სანამ არ გავივლით ყველა
+            ციფრს ორივე რიცხვში.თუ ცვლადი გაუტოლდა 1-ს
+            ყველა დამატების შემდეგ ამ რიცხვს ვამატებთ სტრინგს.
+            საბოლოოდ შედგენილ სტრინგს ვაბრუნებთ და ვიღებთ ორი
+            ბინარული სტრინგის შეკრებას.
+            Time complexity - O(n).
+            Memory complexity - O(n).
+        */
+
+    public String addBinary(String a, String b) {
+        String A = removeLeadingZeros(a);
+        String B = removeLeadingZeros(b);
+        StringBuilder stringBuilder = new StringBuilder();
+        int i = A.length() - 1;
+        int j = B.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry == 1) {
+            if (i >= 0) {
+                carry += A.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                carry += B.charAt(j--) - '0';
+            }
+            stringBuilder.append(carry % 2);
+            carry /= 2;
+        }
+        return stringBuilder.reverse().toString();
+    }
+    private static String removeLeadingZeros(String binaryString) {
+        int index = 0;
+        while (index < binaryString.length() - 1 && binaryString.charAt(index) == '0') {
+            index++;
+        }
+        return binaryString.substring(index);
+    }
+    /*
+        ამოცანა 5.
+            თავდაპირველად ვამოწმებ კიბეების რაოდენობა თუ არის 1 ან 2.
+            ამშემთხვევაში კოდი პირდაპირ დააბრუნებს კიბეების რაოდენობას
+            ვინაიდან ვარიანტების რაოდენობას ემთხვევა.
+            ვიყენებ "Dynamic Programming"-ს, თითეულ ნაბიჯს ვინახავ
+            მასივში სადაც თავდაპირველად გვექნება 1 და 2.
+            ყოველი მომდევნო ნაბიჯი ემთხვევა ფიბონაჩის
+            მიმდევრობას და იქნება წინა ორის ჯამის ტოლი.
+            მასივის ბოლოს კი მივიღებთ ჩვენს საძიებელ რიცხვს.
+        */
+
+    public int countVariants(int stairsCount) {
+        if (stairsCount <=2) return stairsCount;
+
+        int[] steps = new int[stairsCount + 1];
+
+        steps[0] = 1;
+        steps[1] = 2;
+
+        for (int i = 2; i < stairsCount; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+
+        return steps[stairsCount - 1];
+    }
+
+    /*
+        ამოცანა 5.
+            რეკურსიულად
+            Time complexity - O(2^n).
+            Memory complexity - O(1).
+     */
+    public int countVariants2(int stairsCount) {
+        if (stairsCount <= 2) {
+            return stairsCount;
+        }
+        return countVariants2(stairsCount - 1) + countVariants2(stairsCount - 2);
+    }
+
+    /*
+        ამოცანა 5.
+            მემორიზაციით
+            Time complexity - O(n).
+            Memory complexity - O(n).
+     */
+    int[] memo = new int[10000];
+
+    public int countVariants3(int stairsCount) {
+        return countVariantsMemo(stairsCount, memo);
+    }
+
+    private int countVariantsMemo(int stairsCount, int[] memo) {
+        if (stairsCount <= 2) {
+            return stairsCount;
+        }
+        if (memo[stairsCount] != 0) {
+            return memo[stairsCount];
+        }
+        memo[stairsCount] = countVariantsMemo(stairsCount - 1, memo) + countVariantsMemo(stairsCount - 2, memo);
+        return memo[stairsCount];
+    }
+
 
 }
--- src/JavaTasksTest.java
+++ src/JavaTasksTest.java
@@ -5,6 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class JavaTasksTest {
 
     JavaTasks javaTasks = new JavaTasks();
+
+    /*
+        ამოცანა 1.
+     */
+
     @Test
     public void arraySizeIsOne(){
         int[] arr = {1};
@@ -29,6 +34,11 @@ class JavaTasksTest {
         assertEquals(-13 ,javaTasks.singleNumber(arr));
         assertEquals(-13 ,javaTasks.singleNumber2(arr));
     }
+
+    /*
+        ამოცანა 2.
+     */
+
     @Test
     public void minSplitAmount_0(){
         assertEquals(0, javaTasks.minSplit(0));
@@ -46,5 +56,126 @@ class JavaTasksTest {
         assertEquals(29, javaTasks.minSplit(1243));
     }
 
+    /*
+        ამოცანა 3.
+     */
+
+    @Test
+    public void notContains_1(){
+        int[] arr = {-1, 2, 3, -15, -254, 111};
+        assertEquals(   1, javaTasks.notContains(arr));
+    }
+    @Test
+    public void notContains_2(){
+        int[] arr = {181, 28, -17, 3, 178, 1};
+        assertEquals(   2, javaTasks.notContains(arr));
+    }
+    @Test
+    public void notContains_3(){
+        int[] arr = {-12, -14, 1, 2, 5, 13};
+        assertEquals(   3, javaTasks.notContains(arr));
+    }
+    @Test
+    public void notContains_4(){
+        int[] arr = {-123, 1, -15, 2, 13, 3, 124};
+        assertEquals(   4, javaTasks.notContains(arr));
+    }
+    @Test
+    public void notContains_5(){
+        int[] arr = {1, 2, 2, 4, 5};
+        assertEquals(   3, javaTasks.notContains(arr));
+    }
+
+    /*
+        ამოცანა 4.
+     */
+
+    @Test
+    public void addBinary_LeadingZeros(){
+        assertEquals(   "1110", javaTasks.addBinary("0000000011" ,"0000000001011"));
+    }
+    @Test
+    public void addBinary_0_1(){
+        assertEquals(   "1", javaTasks.addBinary("0" ,"1"));
+    }
+    @Test
+    public void addBinary_1_1(){
+        assertEquals(   "10", javaTasks.addBinary("1" ,"1"));
+    }
+    @Test
+    public void addBinary_1010_1011(){
+        assertEquals(   "10101", javaTasks.addBinary("1010" ,"1011"));
+    }
+
+    /*
+        ამოცანა 5.
+     */
+
+    @Test
+    public void countVariants_0(){
+        assertEquals(   0, javaTasks.countVariants(0));
+        assertEquals(0,javaTasks.countVariants2(0));
+        assertEquals(0,javaTasks.countVariants3(0));
+    }
+    @Test
+    public void countVariants_1(){
+        assertEquals(   1, javaTasks.countVariants(1));
+        assertEquals(1,javaTasks.countVariants2(1));
+        assertEquals(1,javaTasks.countVariants3(1));
+    }
+    @Test
+    public void countVariants_2(){
+        assertEquals(  2, javaTasks.countVariants(2));
+        assertEquals(2,javaTasks.countVariants2(2));
+        assertEquals(2,javaTasks.countVariants3(2));
+    }
+    @Test
+    public void countVariants_3(){
+        assertEquals(   3, javaTasks.countVariants(3));
+        assertEquals(3,javaTasks.countVariants2(3));
+        assertEquals(3,javaTasks.countVariants3(3));
+    }
+    @Test
+    public void countVariants_4(){
+        assertEquals(   5, javaTasks.countVariants(4));
+        assertEquals(5,javaTasks.countVariants2(4));
+        assertEquals(5,javaTasks.countVariants3(4));
+    }
+    @Test
+    public void countVariants_5(){
+        assertEquals(   8, javaTasks.countVariants(5));
+        assertEquals(8,javaTasks.countVariants2(5));
+        assertEquals(8,javaTasks.countVariants3(5));
+    }
+    @Test
+    public void countVariants_6(){
+        assertEquals(   13, javaTasks.countVariants(6));
+        assertEquals(13,javaTasks.countVariants2(6));
+        assertEquals(13,javaTasks.countVariants3(6));
+    }
+    @Test
+    public void countVariants_7(){
+        assertEquals(   21, javaTasks.countVariants(7));
+        assertEquals(21,javaTasks.countVariants2(7));
+        assertEquals(21,javaTasks.countVariants3(7));
+    }
+    @Test
+    public void countVariants_8(){
+        assertEquals(   34, javaTasks.countVariants(8));
+        assertEquals(34,javaTasks.countVariants2(8));
+        assertEquals(34,javaTasks.countVariants3(8));
+    }
+    @Test
+    public void countVariants_9(){
+        assertEquals(   55, javaTasks.countVariants(9));
+        assertEquals(55,javaTasks.countVariants2(9));
+        assertEquals(55,javaTasks.countVariants3(9));
+    }
+    @Test
+    public void countVariants_10(){
+        assertEquals(   89, javaTasks.countVariants(10));
+        assertEquals(89,javaTasks.countVariants2(10));
+        assertEquals(89,javaTasks.countVariants3(10));
+    }
 
 }
